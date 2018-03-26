d586fa8663dfffcd5339cb5cc58ab0faaea0bb29
Michael <michael.kuechler2@uzh.ch>
2018-03-27 00:19:30 +0200

    doesnt work

--- task1.c
+++ task1.c
@@ -24,33 +24,43 @@ void maxHeapify(int A[], int i, int n){
 }
 
 void buildMaxHeap(int A[], int n){
-	for(int i = (n-1)/2;i>=0;i--){
+	for(int i = n/2 - 1;i>=0;i--){
 		maxHeapify(A,i,n);
 	}
+	//printHeap(A,n);
 }
 void printHeap(int A[], int n){
-	
+	printf("sorted Array: [");
+	for(int i = 0;i<n;i++){
+		printf("%d, ",A[i]);
+	}
+	printf("]");
 }
 void heapSort(int A[], int n){
 	//create a Heap
 	buildMaxHeap(A,n);
-	for(int i = n-1;i>0;i--){
+	
+	for(int i = n-1;i>=0;i--){
 		//swap first and last element
 		int temp = A[0];
-		A[0] = A[n-1];
-		A[n-1] = temp;
-		n--;
-		//create Heap again
-		maxHeapify(A,0,n);
-		
-		
+		A[0] = A[i];
+		A[i] = temp;
+		//create Heap again	
+		maxHeapify(A,0,i);
 	}
 	
 }
 void printArray(int A[], int n){
+	printf("[");
+	for(int i = 0;i<n-1;++i){
+		printf("%d, ",A[i]);
+	}
+	printf("%d]",A[n-1]);
 	
 }
 int main(){
-	
+	int A[]={3,8,9,1,7};
+	heapSort(A,5);
+	printArray(A,5);
 	return 0;
 }
