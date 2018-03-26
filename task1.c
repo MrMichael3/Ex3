96f9026c62838217ee1e4cae605eb8e4c94e3b35
Michael <michael.kuechler2@uzh.ch>
2018-03-26 22:43:55 +0200

    +buildMaxHeap()

--- task1.c
+++ task1.c
@@ -1,5 +1,43 @@
 //Michael Küchler, 16-924-318
-//33421646+MrMichael3@users.noreply.github.com
+
 #include <stdio.h>
 
+void maxHeapify(int A[], int i, int n){
+	int largest;
+	int l = 2*i;
+	int r = 2*i + 1;
+	if(l <= n && A[l] > A[i]){
+		largest = l;
+	}
+	else{
+		largest = i;
+	}
+	if(r <= n && A[r]>A[largest]){
+		largest = r;
+	}
+	if(largest != i){
+		int temp = A[i];
+		A[i]= A[largest];
+		A[largest] = temp;
+		maxHeapify(A,largest,n);	
+	}
+}
 
+void buildMaxHeap(int A[], int n){
+	for(int i = n/2;i>=0;i--){
+		maxHeapify(A,i,n);
+	}
+}
+void printHeap(int A[], int n){
+	
+}
+void heapSort(int A[], int n){
+	
+}
+void printArray(int A[], int n){
+	
+}
+int main(){
+	
+	return 0;
+}
