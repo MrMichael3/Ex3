ad355a7cb256b6df2dbf4b9a7edb88ede2e82a6d
Michael <michael.kuechler2@uzh.ch>
2018-03-30 21:14:34 +0200

    swap() and printArray()

--- task2a.c
+++ task2a.c
@@ -3,7 +3,10 @@
 #include <stdio.h>
 
 void swap(int A[], int i, int j){
-	
+	int temp;
+	temp = A[i];
+	A[i] = A[j];
+	A[j] = temp;
 }
 int partitionHoare(int A[], int low, int high){
 	return 0;
@@ -12,6 +15,11 @@ void quicksort(int A[], int low, int high){
 
 }
 void printArray(int A[], int size){
+	printf("sorted Array: [");
+	for(int i = 0;i<n-1;i++){
+		printf("A[i], ");
+	}
+	printf("A[n-1]]");
 	
 }
 
