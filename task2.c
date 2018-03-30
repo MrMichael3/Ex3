1673b7c0d0af39199ee5840cd1e298a32bf6aa82
Michael <michael.kuechler2@uzh.ch>
2018-03-30 20:30:27 +0200

    initial

---
+++ task2.c
@@ -0,0 +1,22 @@
+//Michael Küchler, 16-924-318
+
+#include <stdio.h>
+
+void swap(int A[], int i, int j){
+	
+}
+int partitionHoare(int A[], int low, int high){
+	return 0;
+}
+void quicksort(int A[], int low, int high){
+
+}
+void printArray(int A[], int size){
+	
+}
+
+
+int main(){
+	
+	return 0;
+}
